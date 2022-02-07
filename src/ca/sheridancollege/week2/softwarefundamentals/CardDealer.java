c155993b79e1a5d2ec1b1cf67a00b0b7b81a4e6c
Brittzzz34 <98033836+Brittzzz34@users.noreply.github.com>
2022-02-06 21:56:04 -0500

    added comments
    added comments

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -1,13 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Brittany Hanford
+ * ID: 991633307
  */
 package ca.sheridancollege.week2.softwarefundamentals;
 
 /**
  * The class where we create Cards
- * @author dancye, 2019
+ * 
  */
 public class CardDealer 
 {
